0ea4095e53e6d84ecb7d4988996802431f022c22
dreiv <dreivoicu@gmail.com>
2018-06-18 14:30:00 +0300

    moved the item to a separate structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
+import data from './data.json'
 import './App.css'
 
 class App extends Component {
+
+  handleClick() {
+    console.log('Click happened');
+  }
+
+  renderItem = (item, idx) => <li key={idx} onClick={this.handleClick}>{item.name}</li>
+
   render() {
+
     return (
       <div className="App">
         <ul className="grid">
-          <li>Blah</li>
-          <li>Blah</li>
-          <li>Blah</li>
-          <li>Blah</li>
+          {data.map(this.renderItem)}
         </ul>
       </div>
     )

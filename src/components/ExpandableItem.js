74a4f49e7473a8b8a236287589c6a0efeb59611d
dreiv <dreivoicu@gmail.com>
2018-06-18 16:04:49 +0300

    added togglable description

--- src/components/ExpandableItem.js
+++ src/components/ExpandableItem.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 
 export default class ExpandableItem extends Component {
   render() {
+
+    const details = item => <div>{item.description}</div>
+
     return (
       <li className={this.props.isToggledOn ? 'toggledOn' : null}
-          onClick={this.props.onClick}>
+          onClick={this.props.onClick}
+      >
         {this.props.item.name}
+        {this.props.isToggledOn && details(this.props.item)}
       </li>
     );
   }
